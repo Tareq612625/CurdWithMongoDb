8ee537bb80201ff6b697f7de0662e16ac24cdf6c
Tareq Aziz <mdazizkhn@gmail.com>
2023-01-17 12:03:41 +0600

    Add project files.

---
+++ CurdWithMongoDb/Controllers/ProductController.cs
@@ -0,0 +1,87 @@
+﻿using CurdWithMongoDb.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace CurdWithMongoDb.Controllers
+{
+    public class ProductController : Controller
+    {
+        MongoClient client = new MongoClient("mongodb://127.0.0.1:27017");
+        public IActionResult Index()
+        {
+            var database = client.GetDatabase("POS");
+            var table = database.GetCollection<ItemMaster>("Item");
+            var items = table.Find(FilterDefinition<ItemMaster>.Empty).ToList();
+            return View(items);
+        }
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateProduct(ItemMaster obj)
+        {
+
+            var database = client.GetDatabase("POS");
+            var table = database.GetCollection<ItemMaster>("Item");
+            table.InsertOne(obj);
+            ViewBag.msg = "Data save successfully";
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var database = client.GetDatabase("POS");
+            var table = database.GetCollection<ItemMaster>("Item");
+            var product = table.Find(c => c.ItemId == id).FirstOrDefault();
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Edit(ItemMaster obj)
+        {
+
+            var database = client.GetDatabase("POS");
+            var table = database.GetCollection<ItemMaster>("Item");
+            table.ReplaceOne(c => c.ItemId == obj.ItemId, obj);
+            ViewBag.msg = "Data save successfully";
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var database = client.GetDatabase("POS");
+            var table = database.GetCollection<ItemMaster>("Item");
+            var product = table.Find(c => c.ItemId == id).FirstOrDefault();
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Delete(ItemMaster obj)
+        {
+
+            var database = client.GetDatabase("POS");
+            var table = database.GetCollection<ItemMaster>("Item");
+            table.DeleteOne(c => c.ItemId == obj.ItemId);
+            ViewBag.msg = "Data save successfully";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            var database = client.GetDatabase("POS");
+            var table = database.GetCollection<ItemMaster>("Item");
+            var product = table.Find(c => c.ItemId == id).FirstOrDefault();
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+    }
+}
---
+++ CurdWithMongoDb/Models/ItemMaster.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CurdWithMongoDb.Models
+{
+    [BsonIgnoreExtraElements]
+    public class ItemMaster:Common
+    {
+        
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Item id")]
+        public int ItemId { get; set; }
+
+
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "{0} is Required")]
+        [StringLength(maximumLength: 100, ErrorMessage = "{0} length is between {2} and {1}", MinimumLength = 1)]
+        public string? ItemName { get; set; }
+
+
+        [Display(Name = "Description")]
+        [StringLength(maximumLength: 250, ErrorMessage = "{0} length is between {2} and {1}", MinimumLength = 1)]
+        public string? ItemDescription { get; set; }
+
+
+        [Display(Name = "Catagory")]
+        [Required(ErrorMessage = "{0} is Required")]
+        public int CatagoryId { get; set; }
+       
+
+
+        [Display(Name = "Unit")]
+        [Required(ErrorMessage = "{0} is Required")]
+        public int UnitId { get; set; }
+        
+
+        [Display(Name = "Group")]
+        public int GroupId { get; set; }
+      
+
+        [Display(Name = "Keyword")]
+        [StringLength(maximumLength: 100, ErrorMessage = "{0} length is between {2} and {1}", MinimumLength = 1)]
+        public string? ItemKeyword { get; set; }
+
+
+        [Display(Name = "Rate")]
+        [Required(ErrorMessage = "{0} is Required")]
+        public decimal ItemRate { get; set; }
+
+        [Display(Name = "Image")]
+        public string? ItemImage { get; set; }
+    }
+}
